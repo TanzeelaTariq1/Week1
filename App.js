76b4139ab98f8e7fa9568d19f11f73c0d102984d
TanzeelaTariq1 <136908495+TanzeelaTariq1@users.noreply.github.com>
2023-06-21 09:39:39 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
 
 import LittleLemonHeader from './component/LittleLemonHeader';
 import LittleLemonFooter from './component/LittleLemonFooter';
+import MenuItems from './component/MenuItems';
 
 export default function App() {
   return (
     <>
-    <View style={{flex: 1,backgroundColor:'lightgreen'}}>
-      <LittleLemonHeader/>
-    </View>
-    <View style={{flex: 1,justifyContent:'flex-end',backgroundColor:'lightgreen'}}>
-      <LittleLemonFooter/>
-    </View>
+      <View style={styles.container}>
+        <LittleLemonHeader />
+        <TextInput />
+      </View>
+      <View style={styles.footerContainer}>
+        <LittleLemonFooter />
+      </View>
     </>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
