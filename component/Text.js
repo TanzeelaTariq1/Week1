0554b50386354ce62ada0eedd005ca512e7efbc7
ASUS <tanzeelatariq52@gmail.com>
2023-06-22 21:46:56 +0500

    Images/hooks

---
+++ component/Text.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+export default function Text() {
+    return (
+      <>
+      <View>
+      <Text style={{backgroundColor:'white',fontSize:18,fontWeight:'bold'}}>View Menu</Text>
+      </View>
+      <View>
+        <Text style={{backgroundColor:'white',fontSize:18,fontWeight:'bold'}}>View</Text>
+        <Text style={{backgroundColor:'white',fontSize:15}}>View</Text>
+        <Text style={{backgroundColor:'white',fontsize:15}}>View</Text>
+        <Text style={{backgroundColor:'white'}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        <Text style={{backgroundColor:'white',paddingLeft:45}}>View</Text>
+        </View>
+      </>
+    );
+  };
+  
